57f99800cf0ad81622eda0cb5e49a76ed384f70d
Brad Williams <bradwilliams@Brads-MacBook-Pro.local>
2018-12-10 21:07:54 -0800

    master uffff

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,27 @@ listItem.className = "list-group-item list-group-item-action";
 const todoList = document.querySelector('.todoList');
 listItem.textContent = itemName.value
 todoList.appendChild(listItem);
+const deleteBtn = document.createElement('button');
+deleteBtn.textContent = 'Delete'
+deleteBtn.classname = "btn btn-danger"
+deleteBtn.addEventListener('click', deleteItem)
+listItem.appendChild(deleteBtn);
 itemName.value='';
+
 }
 
 const itemName = document.querySelector("#inputToDo");
 const submit = document.querySelector("#submit");
+
 submit.addEventListener('click', ()=>appendlist());
 itemName.addEventListener('change',()=>appendlist());
+
+//more functions
+function deleteItem(){
+    const parentLi = this.parentNode;
+  parentLi.style.display='none';
+   
+    
+alert("triggered");
+};
+
